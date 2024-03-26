b37718647f2e5fb70b73d2c57a4bd4b1f276822a
Felix Schladt <felix.schladt@hensoldt.net>
2024-03-26 14:48:08 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- src/ssd1306_rpc.c
+++ src/ssd1306_rpc.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2022-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
+ */
+
 #include "ssd1306.h"
 #include "fonts.h"
 #include <camkes.h>
